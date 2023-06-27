c02f8da2bd4538a62ff20eb74ffe02048db0d95c
kautsarfrds <firdaus060402@gmail.com>
2023-06-27 11:17:55 +0700

    new pages

---
+++ resources/views/undercons.blade.php
@@ -0,0 +1,164 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Museum Pos Indonesia</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta content="Free HTML Templates" name="keywords" />
+        <meta content="Free HTML Templates" name="description" />
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon" />
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap"
+        rel="stylesheet" />
+
+        <!-- Font Custom -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet" />
+        <!-- Icon Font Stylesheet -->
+        <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        rel="stylesheet" />
+        <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+
+        <!-- Libraries Stylesheet -->
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+        <link
+        href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css"
+        rel="stylesheet" />
+        <link href="lib/lightbox/css/lightbox.min.css"rel="stylesheet" />
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <!-- Navbar Start -->
+        <div class="container-fluid fixed-top bg-light shadow-sm px-5 pe-lg-0">
+        <nav
+            class="navbar navbar-expand-lg bg-light navbar-dark navbar-light py-3 py-lg-0">
+            <a href="index.html" class="navbar-brand">
+            <h1 class="m-2 display-6" style="font-weight: 300">
+                <img src="img/logo.png" alt="" />
+            </h1>
+            </a>
+            <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="index.html" class="nav-item nav-link">Beranda</a>
+                <a href="profil.html" class="nav-item nav-link">Profil</a>
+                <a href="sejarah.html" class="nav-item nav-link">Sejarah</a>
+                <a href="ruangan.html" class="nav-item nav-link">Ruangan</a>
+                <a href="contact.html" class="nav-item nav-link">Kontak</a>
+                <a href="Reservasi.html"class="nav-item nav-link bg-primary text-white px-5 ms-3 d-none d-lg-block">Reservasi <i class="bi bi-arrow-right"></i></a>
+            </div>
+            </div>
+        </nav>
+        </div>
+        <!-- Navbar End -->
+
+        <!-- dashboard Start -->
+       <div class="container my-5">
+        <div class="text-center">
+            <img src="img/undercons.png"  width="500" alt="">
+            <h1 style="font-size: 90px;  color: #ff6600;">404</h1>
+            <h1 style="font-size: 70px;">UNDER CONSTRUCTION</h1>
+        </div>
+
+       </div>
+        <!-- dashboard End -->
+
+        <!-- Footer Start -->
+    <div
+        class="footer container-fluid position-relative bg-dark bg-light-radial text-white-50 py-6 px-5">
+        <div class="row g-5">
+            <div class="col-lg-6 pe-lg-5">
+            <a href="index.html" class="navbar-brand">
+                <h1 class="m-0 display-4 text-uppercase text-white">
+                <i class="bi bi-building text-primary me-2"></i>Meseum Pos indonesia
+                </h1>
+            </a>
+            <p>
+                Sejalan dengan perkembangan perusahaan pos dimana terhitung tanggal 20 juni 1995 nama dan status perusahaan berubah dari Perusahaan Umum Pos dan Giro menjadi PT. Pos Indonesia (persero). maka terjadi pula perubahan nama museum ini dari Museum Pos dan Giro menjadi MUSEUM POS INDONESIA sampai sekarang.
+            </p>
+            <p>
+                <i class="fa fa-map-marker-alt me-2"></i>Jalan Cilaki No.73 Bandung 40115.
+            </p>
+            <p><i class="fa fa-phone-alt me-2"></i>0892383488</p>
+            <p><i class="fa fa-envelope me-2"></i>Meseum pos indonesia.co.id</p>
+            </div>
+            <div class="col-lg-6 ps-lg-5">
+            <div class="row g-5">
+                <div class="col-sm-6">
+                <h4 class="text-white text-uppercase mb-4">Quick Links</h4>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-white-50 mb-2" href="profil.html"><i class="fa fa-angle-right me-2"></i>Profil</a>
+                    <a class="text-white-50 mb-2" href="sejarah.html"><i class="fa fa-angle-right me-2"></i>Sejarah</a>
+                    <a class="text-white-50 mb-2" href="ruangan.html"><i class="fa fa-angle-right me-2"></i>Ruangan</a>
+                    <a class="text-white-50" href="contact.html"><i class="fa fa-angle-right me-2"></i>Kontak</a>
+                </div>
+                <div class="d-flex justify-content-start mt-4">
+                <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2"href="https://twitter.com/i/flow/login"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2"href="https://www.facebook.com/login/" ><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2"href="https://www.linkedin.com/login/in"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-lg btn-primary btn-lg-square rounded-0" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+            </div>
+                </div>
+                </div>
+        <div class="container-fluid bg-dark bg-light-radial text-white border-top border-primary px-0">
+        <div class="d-flex flex-column flex-md-row justify-content-between">
+            <div class="py-4 px-5 text-center text-md-start">
+            <p class="mb-0">&copy; <a class="text-primary" href="https://www.posindonesia.co.id/id/content/museum-pos-indonesia">Pos indonesia.co.id</a>. all Rights Reserved.
+            </p>
+            </div>
+            <div class="py-4 px-5 bg-primary footer-shape position-relative text-center text-md-end">
+            <p class="mb-0"> Designed by <a class="text-dark" href="https://htmlcodex.com">HTML Codex</a>
+            </p>
+            </div>
+        </div>
+        </div>
+        <!-- Footer End -->
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"
+        ><i class="bi bi-arrow-up"></i
+        ></a>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/tempusdominus/js/moment.min.js"></script>
+        <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+        <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+        
+    </body>
+    </html>
+<div>
+    <div> tes</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/artikel', function () {
+    return view('artikel');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
