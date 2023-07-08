692c40b37f1d1d1f44370a5f0f024b3a568ce3c6
NR <nr@Nurul.local>
2023-07-09 03:28:01 +0700

    Routes GM ALL

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,14 +41,34 @@ Route::get('/reservasi', function () {
     return view('reservasi');
 });
 
-Route::get('/artikel', function () {
-    return view('artikel');
+// Route::get('/artikel', function () {
+//     return view('artikel');
+// });
+
+Route::get('/login',[AuthController::class,'login'])->name('login');
+Route::post('/login',[AuthController::class,'authenticating']);
+
+Route::middleware(['auth'])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/dashboard/artikel/add', [DashboardController::class, 'artikelAdd']);
+    Route::get('/dashboard/artikel', [DashboardController::class, 'show']);
+
+    // CRUD Article
+    Route::post('/dashboard/artikel/add', [ArticleController::class, 'store']);
+    Route::get('/dashboard/artikel/{id}', [ArticleController::class, 'detail']);
+    Route::post('/dashboard/artikel/{id}/edit', [ArticleController::class, 'edit']);
+    Route::delete('/dashboard/artikel/{id}/delete', [ArticleController::class, 'delete']);
+    Route::delete('/dashboard/artikel/{id}/arsip', [ArticleController::class, 'arsip']);
+    Route::get('/dashboard/artikel/{id}/restore', [ArticleController::class, 'restore']);
+
+    Route::get('/dashboard/arsip/artikel', [ArticleController::class, 'dataarsip']);
+
+
+    Route::get('/dashboard/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
 
-Route::get('/addArtikel', function () {
-    return view('addArtikel');
-});
+//PUBLIC 
+Route::get('/artikel',[ArticleController::class,'show']);
+Route::get('/artikel/{heading}',[ArticleController::class,'content']);
