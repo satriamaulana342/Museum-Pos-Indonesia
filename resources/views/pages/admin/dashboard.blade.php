8d6b0454eb1d33c42561d5a4d2d6b102dbd2c0aa
kautsarfrds <firdaus060402@gmail.com>
2023-07-12 11:29:30 +0700

    edit dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -53,42 +53,39 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                CRUD Web Museum
             </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Nav Item - Artikel Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="/dashboard/artikel/add" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
+                <a class="nav-link collapsed" href="/dashboard/artikel/add">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Add Artikel</span>
+                    <span>Artikel</span>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
             </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
+            <!-- Nav Item - Profile Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                <a class="nav-link collapsed" href="">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Profile</span>
                 </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
-                </div>
+            </li>
+
+            <!-- Nav Item - Sejarah Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Sejarah</span>
+                </a>
+            </li>
+
+            <!-- Nav Item - Ruangan Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href=""       >
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Ruangan</span>
+                </a>
             </li>
 
             <!-- Divider -->
