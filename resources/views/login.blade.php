aa8eac5a613b60e409f84fb992556e8b0c39a442
kautsarfrds <firdaus060402@gmail.com>
2023-07-26 10:51:02 +0700

    edit login admin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pos Indonesia Dashboard</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+<!-- Template Stylesheet -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
 </head>
 <body>
     <div>
@@ -12,13 +15,22 @@
             <p style="color: brown">{{Session::get('message')}}</p>
         @endif
     </div>
-    <form action="" method="POST">
+    <h1 style="text-align: center">Pos Indonesia Dashboard<br>
+    <img src="{{ url('asset/logo.webp') }}" style="margin-top: 30px; width:150px"/>
+    <br>
+    </h1>
+    <div class="form-container">
+        <form action="" method="POST">
         @csrf
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email">
-        <label for="password">password</label>
-        <input type="password" name="password" id="password">
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+            <label for="email">Email</label><br>
+            <input type="text" name="email" id="email"><br>
+
+            <label for="password">password</label><br>
+            <input type="password" name="password" id="password"><br>
+
+            <button type="submit" class="btn btn-primary" style="margin-top: 10px">Login</button>
+        </form>
+    </div>
+
 </body>
 </html>
